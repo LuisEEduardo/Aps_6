d306b50f6fdb4905f41a05bd49836e4201346214
LuisEEduardo <luiseduardolemos282@gmail.com>
2022-10-09 19:30:15 -0300

    Removendo projeto

---
+++ App/Migrations/20221008173143_CriandoBanco.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace App.Migrations
+{
+    public partial class CriandoBanco : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Dados",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Propriedade = table.Column<string>(type: "varchar(100)", nullable: false),
+                    ProdutoQuimico = table.Column<string>(type: "varchar(100)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Dados", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Usuario",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "varchar(100)", nullable: false),
+                    TipoPermissao = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuario", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Dados_Id",
+                table: "Dados",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Usuario_Id",
+                table: "Usuario",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Dados");
+
+            migrationBuilder.DropTable(
+                name: "Usuario");
+        }
+    }
+}
---
+++ App/Migrations/20221008202410_AddColumnNilvelInformacao.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace App.Migrations
+{
+    public partial class AddColumnNilvelInformacao : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "NivelInformacao",
+                table: "Dados",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NivelInformacao",
+                table: "Dados");
+        }
+    }
+}
