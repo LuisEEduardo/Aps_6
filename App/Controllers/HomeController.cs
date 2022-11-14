89249255adcba95657279fc4913a27646b2bfa95
LuisEEduardo <luiseduardolemos282@gmail.com>
2022-11-14 20:15:41 -0300

    Check point

--- App/Controllers/HomeController.cs
+++ App/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using App.Application;
+using App.DTO;
+using App.DTO.Dados;
 using App.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -14,13 +16,115 @@ namespace App.Controllers
             _dadosAplicacao = dadosAplicacao;
         }
 
-        [HttpGet()]
-        public async Task<IActionResult> Index(int id)
+        //[HttpGet()]
+        //public async Task<IActionResult> Index(int id)
+        //{
+        //    var dados = await _dadosAplicacao.SelecionarTodosDados();
+
+        //    return View(dados);
+        //}
+
+        //[HttpGet()]
+        //public async Task<IActionResult> Index(ReconhecimentoFacialDTO reconhecimentoFacialDTO)
+        //{
+
+        //    using var cliente = new HttpClient();
+
+        //    cliente.BaseAddress = new Uri("http://127.0.0.1:5000");
+
+        //    //var reconhecimentoFacial = new ReconhecimentoFacialDTO()
+        //    //{
+        //    //    FilePath = filePath
+        //    //};
+
+        //    var response = await cliente.PostAsJsonAsync<ReconhecimentoFacialDTO>("/reconhecimento", reconhecimentoFacialDTO);
+
+        //    var contents = await response.Content.ReadAsStringAsync();
+
+        //    var id = int.Parse(contents.Substring(contents.IndexOf("[") + 1, contents.IndexOf(",") - 1).ToString());
+
+        //    var dados = await _dadosAplicacao.SelecionarTodosDados();
+
+        //    return RedirectToAction("Teste", new { dados });
+        //}
+        
+        public async Task<IActionResult> Index([FromBody] string filePath)
         {
+
+            using var cliente = new HttpClient();
+
+            cliente.BaseAddress = new Uri("http://127.0.0.1:5000");
+
+            var reconhecimentoFacial = new ReconhecimentoFacialDTO()
+            {
+                FilePath = filePath
+            };
+
+            var response = await cliente.PostAsJsonAsync<ReconhecimentoFacialDTO>("/reconhecimento", reconhecimentoFacial);
+
+            var contents = await response.Content.ReadAsStringAsync();
+
+            var id = int.Parse(contents.Substring(contents.IndexOf("[") + 1, contents.IndexOf(",") - 1).ToString());
+
             var dados = await _dadosAplicacao.SelecionarTodosDados();
 
-            return View(dados);
+            //return RedirectToAction("Teste", new { dados });
+
+            return Json( new { dados });
         }
 
+        [HttpGet("teste")]
+        public IActionResult Teste(List<DetalharDadoDTO> detalharDadoDTOs)
+        {
+            return View("Index", detalharDadoDTOs);
+        }
+
+
+        //[HttpPost()]
+        //public IActionResult CapturaImagem(string name, [FromServices] IWebHostEnvironment _enviroment)
+        //{
+
+        //    var files = HttpContext.Request.Form.Files;
+        //    string filePath = "";
+
+        //    if (files != null)
+        //    {
+        //        foreach (var file in files)
+        //        {
+        //            if (file.Length > 0)
+        //            {
+        //                var fileName = file.FileName;
+        //                var myUniqueFileName = Convert.ToString(Guid.NewGuid());
+        //                var fileExtension = Path.GetExtension(fileName);
+        //                var newFileName = string.Concat(myUniqueFileName, fileExtension);
+        //                filePath = Path.Combine(_enviroment.WebRootPath, "CameraPhotos") + $@"\{newFileName}";
+        //                if (!string.IsNullOrEmpty(filePath))
+        //                {
+        //                    ArmazenarDiretorio(file, filePath);
+        //                }
+
+        //                //imageBytes = System.IO.File.ReadAllBytes(filePath);                            
+        //            }
+        //        }
+        //    }
+        //    else
+        //    {
+        //        return Json(false);
+        //    }
+
+        //    // return RedirectToAction("Index", "Home", new ReconhecimentoFacialDTO { FilePath = filePath });
+        //    return Json(new ReconhecimentoFacialDTO { FilePath = filePath });
+        //}
+
+
+
+        private void ArmazenarDiretorio(IFormFile file, string fileName)
+        {
+            using (FileStream fs = System.IO.File.Create(fileName))
+            {
+                file.CopyTo(fs);
+                fs.Flush();
+            }
+        }
     }
 }
--- App/Controllers/LoginController.cs
+++ App/Controllers/LoginController.cs
@@ -11,8 +11,86 @@ namespace App.Controllers
             return View();
         }
 
+        //[HttpPost()]
+        //public async Task<IActionResult> CapturaImagem(string name, [FromServices] IWebHostEnvironment _enviroment)
+        //{
+
+        //    var files = HttpContext.Request.Form.Files;
+        //    string filePath = "";
+
+        //    if (files != null)
+        //    {
+        //        foreach (var file in files)
+        //        {
+        //            if (file.Length > 0)
+        //            {
+        //                var fileName = file.FileName;
+        //                var myUniqueFileName = Convert.ToString(Guid.NewGuid());
+        //                var fileExtension = Path.GetExtension(fileName);
+        //                var newFileName = string.Concat(myUniqueFileName, fileExtension);
+        //                filePath = Path.Combine(_enviroment.WebRootPath, "CameraPhotos") + $@"\{newFileName}";
+        //                if (!string.IsNullOrEmpty(filePath))
+        //                {
+        //                    ArmazenarDiretorio(file, filePath);
+        //                }
+
+        //                //imageBytes = System.IO.File.ReadAllBytes(filePath);                            
+        //            }
+        //        }
+        //    }
+        //    else
+        //    {
+        //        return Json(false);
+        //    }
+
+        //    return RedirectToAction("Index", "Home", new ReconhecimentoFacialDTO { FilePath = filePath});
+
+        //    //using var cliente = new HttpClient();
+
+        //    //cliente.BaseAddress = new Uri("http://127.0.0.1:5000");
+
+        //    //var reconhecimentoFacial = new ReconhecimentoFacialDTO()
+        //    //{
+        //    //    FilePath = filePath
+        //    //};
+
+        //    //var response = await cliente.PostAsJsonAsync<ReconhecimentoFacialDTO>("/reconhecimento", reconhecimentoFacial);
+
+        //    //var contents = await response.Content.ReadAsStringAsync();
+
+        //    //var id = int.Parse(contents.Substring(contents.IndexOf("[") + 1, contents.IndexOf(",") - 1).ToString());
+
+        //    //if (response.IsSuccessStatusCode)
+        //    //    return RedirectToAction("Index", "Home", new { id });
+
+        //    //return RedirectToAction("Index");
+        //}
+
+
+        //[HttpGet("reconhecimentoFacial")]
+        //public IActionResult ReconhecimentoFacial(FileStream file)
+        //{
+        //    using var cliente = new HttpClient();
+
+        //    cliente.BaseAddress = new Uri("http://127.0.0.1:5000");
+
+        //    var reconhecimentoFacial = new ReconhecimentoFacialDTO()
+        //    {
+        //        File = file
+        //    };
+
+        //    var post = cliente.PostAsJsonAsync<ReconhecimentoFacialDTO>("reconhecimento", reconhecimentoFacial);
+        //    post.Wait();
+        //    var result = post.Result;
+
+        //    if (result.IsSuccessStatusCode)
+        //        return RedirectToAction("Home", result.Content);
+
+        //    return RedirectToAction("Index");
+        //}
+
         [HttpPost()]
-        public async Task<IActionResult> CapturaImagem(string name, [FromServices] IWebHostEnvironment _enviroment)
+        public IActionResult CapturaImagem(string name, [FromServices] IWebHostEnvironment _enviroment)
         {
 
             var files = HttpContext.Request.Form.Files;
@@ -43,51 +121,10 @@ namespace App.Controllers
                 return Json(false);
             }
 
-            //var fileOpen = new FileStream(filePath, FileMode.Open);
-
-            using var cliente = new HttpClient();
-
-            cliente.BaseAddress = new Uri("http://127.0.0.1:5000");
-
-            var reconhecimentoFacial = new ReconhecimentoFacialDTO()
-            {
-                FilePath = filePath
-            };
-
-            var response = await cliente.PostAsJsonAsync<ReconhecimentoFacialDTO>("/reconhecimento", reconhecimentoFacial);
-
-            var contents = await response.Content.ReadAsStringAsync();
-
-            var id = int.Parse(contents.Substring(contents.IndexOf("[") + 1, contents.IndexOf(",") - 1).ToString());
-
-            if (response.IsSuccessStatusCode)
-                return RedirectToAction("Index", "Home", new { id });
-
-            return RedirectToAction("Index");
+            // return RedirectToAction("Index", "Home", new ReconhecimentoFacialDTO { FilePath = filePath });
+            return Json(filePath);
         }
 
-
-        //[HttpGet("reconhecimentoFacial")]
-        //public IActionResult ReconhecimentoFacial(FileStream file)
-        //{
-        //    using var cliente = new HttpClient();
-
-        //    cliente.BaseAddress = new Uri("http://127.0.0.1:5000");
-
-        //    var reconhecimentoFacial = new ReconhecimentoFacialDTO()
-        //    {
-        //        File = file
-        //    };
-
-        //    var post = cliente.PostAsJsonAsync<ReconhecimentoFacialDTO>("reconhecimento", reconhecimentoFacial);
-        //    post.Wait();
-        //    var result = post.Result;
-
-        //    if (result.IsSuccessStatusCode)
-        //        return RedirectToAction("Home", result.Content);
-
-        //    return RedirectToAction("Index");
-        //}
 
         private void ArmazenarDiretorio(IFormFile file, string fileName)
         {
